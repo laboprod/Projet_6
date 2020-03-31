5469177a2955f486ed4d7b4307e63ba2d691bc0a
laboprod <labeau.francois@gmail.com>
2020-03-31 08:34:48 +0200

    essai

--- move.js
+++ move.js
@@ -1,18 +1,44 @@
+let playerPosition = 'Yoda-cell'.id;
+
 $(document).keydown(function(e) {
 	if (e.which == 39) {
 		// droite
-		player.cellId += 10;
+		playerPosition += 10;
 	}
 	if (e.which == 37) {
 		// gauche
-		player.cellId -= 10;
+		playerPosition -= 10;
 	}
 	if (e.which == 38) {
 		// haut
-		player.cellId -= 1;
+		this.playerPosition -= 1;
 	}
 	if (e.which == 40) {
 		// bas
-		player.cellId += 1;
+		this.playerPosition += 1;
 	}
 });
+
+// $(document).keydown(function(e) {
+// 	let key = e.keyCode || e.which;
+// 	switch (key) {
+// 		case 37:
+// 			//Move left
+// 			$('.Yoda-cell').animate({ left: '-=50px' });
+// 			break;
+// 		case 39:
+// 			//-Move right
+// 			$('.Yoda-cell').animate({ left: '+=50px' });
+// 			break;
+// 		case 38:
+// 			//-Move up
+// 			$('.Yoda-cell').animate({ top: '-=50px' });
+// 			break;
+// 		case 40:
+// 			//-Move down
+// 			$('.Yoda-cell').animate({ top: '+=50px' });
+// 			break;
+// 		default:
+// 			break;
+// 	}
+// });
