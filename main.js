addc05751aa52f36ee0cc6f34276a56951949c0f
laboprod <labeau.francois@gmail.com>
2020-04-27 10:00:40 +0200

    split with Game.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let game = new Game();
 
-$('.content').hide();
-$('#enterButton').click(function () {
-	$('#music')[0].play();
-	$('#intro').hide();
-	$('.content').show();
-});
+// $('.content').hide();
+// $('#enterButton').click(function () {
+// 	$('#music')[0].play();
+// 	$('#intro').hide();
+// 	$('.content').show();
+// });
+
+$('.content').show();
 
 $('.nameForm').submit(function (e) {
 	e.preventDefault();
