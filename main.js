8dc858178c61bc14e4fe765b51de470f64ba5832
laboprod <labeau.francois@gmail.com>
2020-05-14 13:00:50 +0200

    add playerId

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let game = new Game();
-
+let playerId = 0;
 // $('.content').hide();
 // $('#enterButton').click(function () {
 // 	$('#music')[0].play();
@@ -10,10 +10,11 @@ let game = new Game();
 $('.content').show();
 
 $('.nameForm').submit(function (e) {
+	playerId++;
 	e.preventDefault();
 	let side = $(this).attr('id');
 	let name = e.target.querySelector('.playerName').value;
-	let player = new Player(name, side);
+	let player = new Player(name, side, playerId);
 	game.addPlayer(player);
 	if (game.isReady()) {
 		game.start();
