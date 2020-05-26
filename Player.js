ca69036969aa2b4c280686a582293ea7d5736bd8
laboprod <labeau.francois@gmail.com>
2020-05-26 16:50:21 +0200

    add attack(), defend() and win()

--- Player.js
+++ Player.js
@@ -3,8 +3,7 @@ class Player {
 	weapon;
 	health;
 	attackCount;
-	attack;
-	defend;
+	defending;
 	plateau;
 	position;
 	moveCount;
@@ -15,81 +14,51 @@ class Player {
 		this.weapon = new Weapon('pistol', 10);
 		this.health = 100;
 		this.attackCount = 0;
-		this.attack = false;
-		this.defend = false;
+		this.defending = false;
 		this.position = null;
 		this.moveCount = 0;
 	}
 
-	attack() {
-		this.attack = true;
+	defend() {
 		this.attackCount++;
-
-		let currentPlayer = game.currentPlayer;
-		let nextPlayer = game.otherPlayer;
-
-		if (!nextPlayer.defend) {
-			//si le joueur adverse ne se defend pas
-			nextPlayer.health = nextPlayer.health - currentPlayer.weapon.damage;
-		} else {
-			//si le joueur adverse se defend
-			nextPlayer.health = nextPlayer.health - currentPlayer.weapon.damage / 2;
-		}
-		$('#' + nextPlayer.id + ' #pb-player')
-			.css('width', nextPlayer.health + '%')
-			.text(nextPlayer.health); // met a jour la barre de vie
+		this.defending = true;
+		this.resetAttackCount();
 	}
 
-	canAttack() {
-		if (this.attackCount > 0) {
+	attack(otherPlayer) {
+		this.attackCount++;
+		this.defending = false;
+
+		if (!otherPlayer.defending) {
+			//si le joueur adverse ne se defend pas
+			otherPlayer.health = otherPlayer.health - this.weapon.damage;
+		} else {
+			otherPlayer.health = otherPlayer.health - this.weapon.damage / 2;
+		}
+		if (otherPlayer.health < 0) {
+			otherPlayer.health = 0;
+		}
+
+		$('#' + otherPlayer.id + ' #pb-player')
+			.css('width', otherPlayer.health + '%')
+			.text(otherPlayer.health);
+
+		if (otherPlayer.health == 0) {
+			this.win();
+			return;
+		}
+		this.resetAttackCount();
+	}
+
+	canMove() {
+		if (game.fighting) {
+			return false;
+		}
+		if (this.moveCount >= game.moveMaxAllowed) {
 			return false;
 		}
 		return true;
 	}
-
-	// battle() {
-	// 	let currentPlayerNumber = this.id;
-	// 	let nextPlayer = game.otherPlayer();
-
-	// 	// desactivateButtons();
-
-	// 	$('.showPlayer' + currentPlayerNumber).addClass('highLight'); // highlight en jaune les boutons du joueur qui joue
-	// 	$('#player' + currentPlayerNumber + 'Att').click(function () {
-	// 		this.attack = false;
-
-	// 		if (this.currentPlayer.canAttack()) {
-	// 			this.currentPlayer.attack();
-	// 			this.currentPlayer.resetAttackCount();
-	// 		}
-
-	// 		if (nextPlayer.health > 0) {
-	// 			this.resetAttackCount();
-	// 			game.changePlayer();
-	// 		} else {
-	// 			this.currentPlayer.win();
-	// 		}
-	// 	});
-	// 	$('#player' + currentPlayerNumber + 'Def').click(function () {
-	// 		// si on se defend
-	// 		this.attackCount++;
-	// 		this.defend = true;
-	// 		game.changePlayer();
-	// 	});
-	// }
-
-	// win() {
-	// 	let nextPlayerNumber = nextPlayer.id;
-	// 	let currentPlayerNumber = this.id;
-
-	// 	$('#pb-player' + nextPlayerNumber)
-	// 		.text('0')
-	// 		.css('width', '0%');
-	// 	$('.showPlayer' + currentPlayerNumber).removeClass('highlight');
-	// 	setTimeout(function () {
-	// 		alert(`${this.name} a gagné le combat !`);
-	// 	}, 1000);
-	// 	$('#restartGame').show();
-	// }
 
 	canMoveDown() {
 		let cell = this.position;
@@ -148,9 +117,10 @@ class Player {
 		return true;
 	}
 
-	desactivateButtons() {
-		$('#player' + this.id + 'Att', '#player' + this.id + 'Def').off('click');
-		$('.showPlayer' + this.id).removeClass('highLight');
+	highlight() {
+		$('#' + this.id).addClass('highlight-current-player');
+		$('#' + this.id + ' .moves').text('Reste : ' + (game.moveMaxAllowed - this.moveCount) + ' déplacements ');
+		$('#' + this.id + ' .turn').text('A toi de jouer !');
 	}
 
 	moveDown() {
@@ -204,7 +174,6 @@ class Player {
 	place(cell, plateau) {
 		this.position = cell;
 		this.plateau = plateau;
-		this.desactivateButtons();
 	}
 
 	resetAttackCount() {
@@ -214,48 +183,13 @@ class Player {
 	resetMoveCount() {
 		this.moveCount = 0;
 	}
+
+	win() {
+		setTimeout(() => {
+			alert(this.name + ' a gagné le combat !');
+		}, 1000);
+		$('.btn').hide();
+		$('.turn').hide();
+		$('#restartGame').fadeIn(2000);
+	}
 }
-
-// 	recoitCoupPar(attaquant) {
-// 		this.nbreCoupRecu++;
-
-// 		if (!this.estVivant) {
-// 			throw new Error(this.name + ' est deja mort');
-// 		}
-// 		if (this.name == attaquant.name) {
-// 			throw new Error('je ne peux pas mattaquer moi-meme');
-// 		}
-// 		if (this.defend === false) {
-// 			this.health -= attaquant.weapon.damage;
-// 		} else {
-// 			this.health -= attaquant.weapon.damage / 2;
-// 		}
-
-// 		this.afficher(`
-// 		${this.name} recoit un coup par ${attaquant.name}`);
-
-// 		if (this.health < 0) {
-// 			this.health = 0;
-// 			if (this.nbreCoupRecu > 1) {
-// 				this.afficher(`${this.name} est mort après ${this.nbreCoupRecu} coups.`);
-// 			} else {
-// 				this.afficher(`${this.name} est mort après ${this.nbreCoupRecu} coup.`);
-// 			}
-// 			return;
-// 		}
-
-// 		return this.health;
-// 	}
-// 	get estVivant() {
-// 		if (this.health <= 0) {
-// 			return false;
-// 		}
-// 		return true;
-// 	}
-
-// 	afficher(message) {
-// 		document.querySelector('body').innerHTML += message;
-// 	}
-// 	getNewWeapon(weapon) {
-// 		this.weapon = weapon;
-// 	}
