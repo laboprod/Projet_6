e005bd27998df1566b90e683f65e099c9d25b1e6
laboprod <labeau.francois@gmail.com>
2020-06-08 14:37:45 +0200

    add create and display weapon()

--- Game.js
+++ Game.js
@@ -1,12 +1,14 @@
 class Game {
-	constructor(playersQty = 2) {
-		this.playersQty = playersQty;
+	constructor(weaponsQty = 4) {
+		this.playersQty = 2;
+		this.weaponsQty = weaponsQty;
 		this.players = [];
 		this.weapons = [];
 		this.turnPlayerIndex = 0;
 		this.moveMaxAllowed = 3;
 		this.plateau = null;
 		this.fighting = false;
+		this.possibleWeapons = ['rifle', 'canon', 'sniper', 'lightsaber'];
 	}
 
 	get currentPlayer() {
@@ -21,8 +23,8 @@ class Game {
 		return this.players[0];
 	}
 
-	isReady() {
-		return this.players.length === this.playersQty;
+	addPlayer(player) {
+		this.players.push(player);
 	}
 
 	battle() {
@@ -41,27 +43,39 @@ class Game {
 		});
 	}
 
-	addPlayer(player) {
-		this.players.push(player);
+	changePlayer() {
+		if (this.turnPlayerIndex === 1) {
+			this.turnPlayerIndex = 0;
+		} else {
+			this.turnPlayerIndex = 1;
+		}
+
+		$('#' + this.currentPlayer.id).addClass('highlight-current-player');
+		$('#' + this.otherPlayer.id).removeClass('highlight-current-player');
+
+		$('.' + this.currentPlayer.id + '-cell').addClass(this.currentPlayer.id + '-cell-blink');
+		$('.' + this.otherPlayer.id + '-cell').removeClass(this.otherPlayer.id + '-cell-blink');
+
+		this.currentPlayer.resetMoveCount();
+	}
+
+	changeWeapon(newWeapon) {
+		let actualWeapon = this.currentPlayer.weapon;
+		let weapons = this.weapons.filter(function (weapon) {
+			return weapon.position !== newWeapon.position;
+		});
+		this.currentPlayer.weapon = newWeapon;
+		actualWeapon.position = this.currentPlayer.position;
+		weapons.push(actualWeapon);
+		this.weapons = weapons;
+		this.plateau.changeWeaponType(newWeapon, actualWeapon);
+		this.refreshWeapon();
 	}
 
 	createWeapons() {
-		this.weapons.push(new Weapon('rifle', 20));
-		this.weapons.push(new Weapon('canon', 30));
-		this.weapons.push(new Weapon('sniper', 40));
-		this.weapons.push(new Weapon('lightsaber', 50));
-
-		for (let index in this.weapons) {
-			this.plateau.placeWeapon(this.weapons[index]);
-		}
-	}
-
-	displayPlayers() {
-		for (let player of this.players) {
-			$('#' + player.id + ' .playerName').text(player.name);
-			$('#' + player.id + ' .weapon').text('Arme : ' + player.weapon.name + ' - Dégâts : ' + player.weapon.damage);
-
-			this.plateau.placePlayer(player);
+		for (let i = 0; i < this.weaponsQty; i++) {
+			let weapon = this.generateUniqueWeapon();
+			this.weapons.push(new Weapon(weapon.name, weapon.damage));
 		}
 	}
 
@@ -76,30 +90,81 @@ class Game {
 		$('.btn').fadeIn(1000);
 	}
 
+	displayPlayers() {
+		for (let player of this.players) {
+			$('#' + player.id + ' .playerName').text(player.name);
+			$('#' + player.id + ' .weapon').text('Arme : ' + player.weapon.name + ' - Dégâts : ' + player.weapon.damage);
+
+			this.plateau.placePlayer(player);
+		}
+	}
+
+	displayWeapons() {
+		for (let index in this.weapons) {
+			this.plateau.placeWeapon(this.weapons[index]);
+		}
+	}
+
+	generateUniqueWeapon() {
+		let damage = Math.round(Math.random() * 100);
+		let index = Math.floor(Math.random() * this.possibleWeapons.length);
+		let name = this.possibleWeapons[index];
+		let weapon = new Weapon(name, damage);
+
+		if (this.weapons.includes(weapon)) {
+			return generateUniqueWeapon();
+		}
+		return weapon;
+	}
+
+	getNewWeapon() {
+		let playerPosition = this.currentPlayer.position;
+		let weapon = this.weapons.filter(function (weapon) {
+			return weapon.position == playerPosition;
+		});
+		if (weapon.length) {
+			this.changeWeapon(weapon[0]);
+		}
+	}
+
+	isEnnemyClose() {
+		if (this.currentPlayer.position + 1 == this.otherPlayer.position) {
+			return true;
+		}
+		if (this.currentPlayer.position - 1 == this.otherPlayer.position) {
+			return true;
+		}
+		if (this.currentPlayer.position + 10 == this.otherPlayer.position) {
+			return true;
+		}
+		if (this.currentPlayer.position - 10 == this.otherPlayer.position) {
+			return true;
+		}
+		if (this.otherPlayer.position + 1 == this.currentPlayer.position) {
+			return true;
+		}
+		if (this.otherPlayer.position - 1 == this.currentPlayer.position) {
+			return true;
+		}
+		if (this.otherPlayer.position + 10 == this.currentPlayer.position) {
+			return true;
+		}
+		if (this.otherPlayer.position - 10 == this.currentPlayer.position) {
+			return true;
+		}
+		return false;
+	}
+
+	isReady() {
+		return this.players.length === this.playersQty;
+	}
+
 	intro() {
 		// $('#music')[0].pause();
 		$('#game').fadeIn(2000); // affiche le jeu
 		$('.btn').hide();
 		$('#battle').hide();
 		$('#restartGame').hide(); // cache le bouton recommencer tant que la partie n'est pas fini
-	}
-
-	start() {
-		this.intro();
-
-		this.plateau = new Plateau(10, 10, this); //initialiser le plateau avec 10 rangées et 10 colonnes.
-		this.plateau.generer();
-		this.plateau.placeBlocks(10); // cree 10 cases grises
-
-		this.createWeapons();
-		this.displayPlayers();
-
-		$('#nameform1').hide(); // cache la page où l'on choisi son nom
-		$('#nameform2').hide();
-
-		this.currentPlayer.highlight();
-
-		document.addEventListener('keydown', (e) => this.listenForKeyStroke(e));
 	}
 
 	listenForKeyStroke(e) {
@@ -141,70 +206,22 @@ class Game {
 		}
 	}
 
-	changeWeapon(newWeapon) {
-		let actualWeapon = this.currentPlayer.weapon;
-		let weapons = this.weapons.filter(function (weapon) {
-			return weapon.position !== newWeapon.position;
-		});
-		this.currentPlayer.weapon = newWeapon;
-		actualWeapon.position = this.currentPlayer.position;
-		weapons.push(actualWeapon);
-		this.weapons = weapons;
-		this.plateau.changeWeaponType(newWeapon, actualWeapon);
-		this.refreshWeapon();
-	}
+	start() {
+		this.intro();
 
-	getNewWeapon() {
-		let playerPosition = this.currentPlayer.position;
-		let weapon = this.weapons.filter(function (weapon) {
-			return weapon.position == playerPosition;
-		});
-		if (weapon.length) {
-			this.changeWeapon(weapon[0]);
-		}
-	}
+		this.plateau = new Plateau(10, 10, this); //initialiser le plateau avec 10 rangées et 10 colonnes.
+		this.plateau.generer();
+		this.plateau.placeBlocks(10); // cree 10 cases grises
 
-	isEnnemyClose() {
-		if (this.currentPlayer.position + 1 == this.otherPlayer.position) {
-			return true;
-		}
-		if (this.currentPlayer.position - 1 == this.otherPlayer.position) {
-			return true;
-		}
-		if (this.currentPlayer.position + 10 == this.otherPlayer.position) {
-			return true;
-		}
-		if (this.currentPlayer.position - 10 == this.otherPlayer.position) {
-			return true;
-		}
-		if (this.otherPlayer.position + 1 == this.currentPlayer.position) {
-			return true;
-		}
-		if (this.otherPlayer.position - 1 == this.currentPlayer.position) {
-			return true;
-		}
-		if (this.otherPlayer.position + 10 == this.currentPlayer.position) {
-			return true;
-		}
-		if (this.otherPlayer.position - 10 == this.currentPlayer.position) {
-			return true;
-		}
-		return false;
-	}
+		this.createWeapons();
+		this.displayWeapons();
+		this.displayPlayers();
 
-	changePlayer() {
-		if (this.turnPlayerIndex === 1) {
-			this.turnPlayerIndex = 0;
-		} else {
-			this.turnPlayerIndex = 1;
-		}
+		$('#nameform1').hide(); // cache la page où l'on choisi son nom
+		$('#nameform2').hide();
 
-		$('#' + this.currentPlayer.id).addClass('highlight-current-player');
-		$('#' + this.otherPlayer.id).removeClass('highlight-current-player');
+		this.currentPlayer.highlight();
 
-		$('.' + this.currentPlayer.id + '-cell').addClass(this.currentPlayer.id + '-cell-blink');
-		$('.' + this.otherPlayer.id + '-cell').removeClass(this.otherPlayer.id + '-cell-blink');
-
-		this.currentPlayer.resetMoveCount();
+		document.addEventListener('keydown', (e) => this.listenForKeyStroke(e));
 	}
 }
