af749222066818f17efbea9e9832a20fed109ca6
laboprod <labeau.francois@gmail.com>
2020-06-15 09:35:14 +0200

    get directions

--- Cell.js
+++ Cell.js
@@ -3,4 +3,36 @@ class Cell {
 		this.x = x;
 		this.y = y;
 	}
+
+	get cellDom() {
+		return $(`.cell[data-x=${this.x}][data-y=${this.y}]`);
+	}
+
+	get west() {
+		let x = this.x - 1;
+		let y = this.y;
+
+		return new Cell(x, y);
+	}
+
+	get east() {
+		let x = this.x + 1;
+		let y = this.y;
+
+		return new Cell(x, y);
+	}
+
+	get north() {
+		let x = this.x;
+		let y = this.y - 1;
+
+		return new Cell(x, y);
+	}
+
+	get south() {
+		let x = this.x;
+		let y = this.y + 1;
+
+		return new Cell(x, y);
+	}
 }
